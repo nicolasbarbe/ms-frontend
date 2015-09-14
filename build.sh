d3083d19a029f7fc75d3b4f5c312a94158a0b982
Nicolas Barbé <nicolas.barbe@magnolia-cms.com>
2015-09-14 17:36:58 +0200

    Automatically push the image to docker hub in the build.sh script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mvn clean install
 eval $(boot2docker shellinit)
 docker build -t nicolasbarbe/ms-frontend:1.0-SNAPSHOT .
-#docker push nicolasbarbe/ms-frontend:1.0-SNAPSHOT
+docker push nicolasbarbe/ms-frontend:1.0-SNAPSHOT
